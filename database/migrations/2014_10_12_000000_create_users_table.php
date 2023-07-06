9606ab25eab950ffad2e924108b2ba7ab27abca5
ketketin <114962909+ketketin@users.noreply.github.com>
2023-07-06 21:31:49 +0700

    poin membership

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer("membership");
+            $table->integer("poin");
             $table->enum('role', ['customer', 'owner', 'staff']);
             $table->rememberToken();
             $table->timestamps();
