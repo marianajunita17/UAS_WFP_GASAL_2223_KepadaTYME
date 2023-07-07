45ce02ccb937c08527d812a685d7e83966129dfe
marianajunita17 <ymawuntu14@gmail.com>
2023-07-07 14:39:56 +0700

    product
    h

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Brand;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class ProductSeeder extends Seeder
@@ -13,6 +15,17 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Factory::create();
+        $types = Type::get();
+        $brands = Brand::get();
+        for ($i=0; $i < 20; $i++) {
+            $p = new Product();
+            $p->name = $faker->word();
+            $p->price = $faker->numberBetween(10, 20000) * 100;
+            $p->filename = "https://picsum.photos/id/".$faker->numberBetween(1,1060)."/300/300";
+            $p->types_id = $faker->randomElement($types)->id;
+            $p->brands_id = $faker->randomElement($brands)->id;
+            $p->save();
+        }
     }
 }
