93332c122e2473eac60a75af310d0987c7ff6647
ketketin <114962909+ketketin@users.noreply.github.com>
2023-07-07 13:08:13 +0700

    seeder&model

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Faker\Factory;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Factory::create();
+        for ($i=0; $i < 5; $i++) {
+            $category = new Category();
+            $category->name = $faker->word();
+            $category->save();
+        }
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // User::factory(10)->create();
+        $this->call([
+            CategorySeeder::class,
+            ProductSeeder::class,
+            UserSeeder::class,
+            TransactionSeeder::class
+        ]);
     }
 }
